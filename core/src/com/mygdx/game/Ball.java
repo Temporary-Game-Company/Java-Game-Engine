b34dc40600238da7adbf066e7d5c4db4ee87b8e4
Jordan <mrjordanmarcus@gmail.com>
2021-01-04 16:32:22 -0500

    Increased ball momentum

--- core/src/com/mygdx/game/Ball.java
+++ core/src/com/mygdx/game/Ball.java
@@ -28,7 +28,7 @@ public class Ball {
         bodyDef.position.set(x, y);
 
         this.ballBody = world.createBody(bodyDef);
-        ballBody.setLinearDamping(6f);  // How fast ball stops moving stops.
+        ballBody.setLinearDamping(10f);  // How fast ball stops moving stops.
 
         CircleShape ball = new CircleShape();
         ball.setRadius(size);
