c5da43746bbf62e3c96d3211f28c3f3b381b13b7
Joshua-E-Weinstein <joshua.e.weinstein@gmail.com>
2021-01-09 18:53:47 -0500

    Comments!!!!!

--- core/assets/Maps/Placeholders.tsx
+++ core/assets/Maps/Placeholders.tsx
@@ -1,4 +1,16 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="Placeholders" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
  <image source="../Images/Debug Tiles.png" width="512" height="512"/>
+ <terraintypes>
+  <terrain name="Red Cube" tile="34"/>
+  <terrain name="Blue" tile="38"/>
+ </terraintypes>
+ <tile id="1" terrain="0,0,0,0"/>
+ <tile id="4" terrain="0,0,0,0"/>
+ <tile id="5" terrain="1,1,1,1"/>
+ <tile id="8" terrain="1,1,1,1"/>
+ <tile id="97" terrain="0,0,0,0"/>
+ <tile id="100" terrain="0,0,0,0"/>
+ <tile id="101" terrain="1,1,1,1"/>
+ <tile id="104" terrain="1,1,1,1"/>
 </tileset>
